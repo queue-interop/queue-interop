fce13e29edb56869db39099bf346b3cc859aae37
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2017-08-02 17:54:06 +0300

    add provider does not support helper methods.

--- src/DeliveryDelayNotSupportedException.php
+++ src/DeliveryDelayNotSupportedException.php
@@ -4,4 +4,14 @@ namespace Interop\Queue;
 
 class DeliveryDelayNotSupportedException extends Exception
 {
+    /**
+     * @param int $code
+     * @param \Throwable $previous
+     *
+     * @return static
+     */
+    public static function providerDoestNotSupportIt($code = 0, $previous = null)
+    {
+        return new static('The provider does not support delivery delay feature', $code, $previous);
+    }
 }
--- src/PriorityNotSupportedException.php
+++ src/PriorityNotSupportedException.php
@@ -4,4 +4,14 @@ namespace Interop\Queue;
 
 class PriorityNotSupportedException extends Exception
 {
+    /**
+     * @param int $code
+     * @param \Throwable $previous
+     *
+     * @return static
+     */
+    public static function providerDoestNotSupportIt($code = 0, $previous = null)
+    {
+        return new static('The provider does not support priority feature', $code, $previous);
+    }
 }
