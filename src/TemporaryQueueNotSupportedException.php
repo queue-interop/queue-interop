dd577d7ce3174b49ea56a90463d00933f0eb37e0
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2018-08-22 14:27:19 +0300

    add temporary queue not supported exception.

---
+++ src/TemporaryQueueNotSupportedException.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace Interop\Queue;
+
+class TemporaryQueueNotSupportedException extends Exception
+{
+    /**
+     * @param int $code
+     * @param \Throwable $previous
+     *
+     * @return static
+     */
+    public static function providerDoestNotSupportIt(int $code = 0, \Throwable $previous = null): self
+    {
+        return new static('The provider does not support temporary queue feature', $code, $previous);
+    }
+}
